3ef74edeeaa56343549bd6ad6ee3bbbe0b197db0
MarceloSilSantos <tecmarcelo78@outlook.com>
2024-09-06 16:25:13 -0300

    novo projeto integracao

---
+++ src/Quote.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Quote() {
+    // Implement quote logic here
+    return <div>Quote Screen</div>;
+}
+
+export default Quote;
---
+++ src/ServiceProvider.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function ServiceProvider() {
+    // Implement service provider logic here
+    return <div>Service Provider Screen</div>;
+}
+
+export default ServiceProvider;
